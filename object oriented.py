f891582189414705d68f8c1344f8fd85e32ad0e9
Thomas Bassett <tom-bassett@hotmail.co.uk>
2014-12-08 10:42:47 +0000

    extended

--- object oriented.py
+++ object oriented.py
@@ -1,22 +1,22 @@
-entry = raw_input("Would you like to see wage or name? ")
-
-class Employee:
-    def __init__(self):
-        self.name="Dom"
-        self.wage="30000 Pounds"
-
-myEmployee = Employee()
-
-if entry == "Name":
-    print myEmployee.name
-    wage = raw_input("Would you like to see their wage?")
-    if wage == "Yes":
-        print myEmployee.Wage
-        exit()
+def program():
+    entry = raw_input("Would you like to see wage or name(Capital Letter start)?")
+    class Employee:
+        def __init__(self):
+            self.name=["Dom"]
+            self.wage="30000 Pounds"
+    myEmployee = Employee()
+    if entry == "Name":
+        print myEmployee.name
+        wage = raw_input("Would you like to see their wage?")
     else:
-        print "Okay fine....suit yourself"
-        exit()
-else:
-    print myEmployee.wage
-    exit()
+        print myEmployee.wage
+        new = raw_input("Enter a new employee name:")
+        myEmployee.name.append(new)
+        print myEmployee.name
+
+for i in range(1,100):
+    program()
+
+    
+
 
