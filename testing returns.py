af750ed958f90a4f2b28deba351d0eb6101b83af
Thomas Bassett <tom-bassett@hotmail.co.uk>
2015-01-05 09:46:35 +0000

    countdown

--- testing returns.py
+++ testing returns.py
@@ -1,8 +1,12 @@
+count=0
 def power(n,p):
+    global count
+    count+=1
     if p==0:
         return 1
     else:
         return n*power(n,p-1)
     
 if __name__=='__main__':
-    print power(2,3)
+    print power(2,0)
+    print count
