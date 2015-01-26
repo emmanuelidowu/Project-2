d5a624b453119567afb1dddedf5722f5be21c9a2
Thomas Bassett <tom-bassett@hotmail.co.uk>
2015-01-26 15:09:44 +0000

    update

--- paininthearse.py
+++ paininthearse.py
@@ -21,23 +21,19 @@ data = json.loads(response)
 
 
 def menu_FP_name():
-    count = 0
     name_enter = raw_input("Please enter the company name you would like")
     for item in data['rows']:
         FPurl2 = "http://dev.c0l.in:5984/financial_positions/" + item['id']
         response2 = urllib2.urlopen(FPurl2).read()
         data2 = json.loads(response2)
-        if data2['company']['name']== name_enter:
-            count=count+1
-            if count >5000:
-                break
-            fieldnames = ['Company Name', 'Non Current Assets','Current Assets','Total Assets']
-            #csvfile1=open('dataa.csv',"wb")
-            #datacsv.writerow({'Company Name': data2['company']['name'],
-                             #'Non Current Assets': data2['company']['non_current_assets'],
-                             #'Current Assets':data2['company']['current_assets'],
-                             #'Total Assets':data2['company']['non_current_assets']+data2['company']['current_assets']})
-            print count
+        if 'sector' in data2:
+            if data2['company']['name']== name_enter:
+                print data2['company']['name'], ' ID number....', data2['id']
+                #with open('names.csv','w+') as csvfile:
+                    #fieldnames = ['Company Name', 'Non Current Assets','Current Assets','Total Assets']
+                    #writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+                    #writer.writerow({'Company Name': data2['company']['name'],'Non Current Assets': data2['company']['non_current_assets'],'Current Assets':data2['company']['current_assets'],'Total Assets':data2['company']['non_current_assets']+ data2['company']['current_assets']})
+            
     
         
 
@@ -49,19 +45,9 @@ def menu_FP():
         FPurl2 = "http://dev.c0l.in:5984/financial_positions/" + item['id']
         response2 = urllib2.urlopen(FPurl2).read()
         data2 = json.loads(response2)
-        if data2['id']==choice_FP1:
-            print data2['company']['name']
-
-    choice_FP2 = raw_input("""Please select one of the following the view it:
-    1.Company Name
-    2.Company Sector
-    3.Non current assets
-    4.Current assests
-    5.Total assets
-    6.Equity
-    7.Non current liabilities
-    8.Current liabilities
-    9.Total Equity & Liabilities""")    
+        if 'sector' in data2:
+            if data2['id']==choice_FP1:
+                print data2['company']['name']+" ID number...",data2['id']  
     
 def financial_positions():
 
@@ -113,12 +99,14 @@ def income_statement():
         if 'sector' in Idata2:
             if Idata2['sector'] == user_inputI:
                 print Idata2['company']['name']," ID..",Idata2['id']
+        #with open("data.csv",'wb')
     print"--------------------------"
     menu_FP() 
 
 print "please select one of the following that you would like to view. (1 or 2)"
 choice_1=raw_input("""1. Financial Position 
 2. Income Statement
+3. Search by name with the Financial Position data
 """)
 if choice_1 =="1":
     financial_positions()
